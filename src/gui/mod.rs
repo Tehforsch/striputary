b6003c5db12e9e267e86bc3ef93dd7be1ab9022b
tehforsch <mail@tonipeter.de>
2021-07-23 11:33:15 +0200

    start thread for cutting instead of running on main thread

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -3,6 +3,8 @@ mod graphics;
 mod input;
 mod offset_marker;
 
+use std::thread;
+
 use self::{
     graphics::{
         camera_positioning_system, initialize_camera_system, marker_positioning_system,
@@ -15,12 +17,7 @@ use self::{
     },
     offset_marker::PositionMarker,
 };
-use crate::{
-    audio_excerpt::AudioExcerpt,
-    config::NUM_OFFSETS_TO_TRY,
-    cut::{cut_song, get_named_excerpts, NamedExcerpt},
-    recording_session::RecordingSession,
-};
+use crate::{audio_excerpt::AudioExcerpt, audio_time::AudioTime, config::NUM_OFFSETS_TO_TRY, cut::{cut_song, get_named_excerpts, NamedExcerpt}, recording_session::RecordingSession, song::Song};
 use bevy::prelude::*;
 use bevy_prototype_lyon::plugin::ShapePlugin;
 
@@ -41,7 +38,7 @@ pub fn run(session: RecordingSession) {
         .add_system(scrolling_input_system.system())
         .add_system(spawn_offset_markers_system.system())
         .add_system(exit_system.system())
-        .add_system(cut_system.system())
+        .add_system(start_cut_system.system())
         .add_system(track_mouse_position_system.system())
         .add_system(move_markers_on_click_system.system())
         .add_system(marker_positioning_system.system())
@@ -63,7 +60,13 @@ fn get_volume_data(excerpt: &AudioExcerpt) -> Vec<f64> {
     times.map(|time| excerpt.get_volume_at(time)).collect()
 }
 
-fn cut_system(
+struct CutInfo {
+    song: Song,
+    start_time: f64,
+    end_time: f64,
+}
+
+fn start_cut_system(
     keyboard_input: Res<Input<KeyCode>>,
     session: Res<RecordingSession>,
     positions: Query<&PositionMarker>,
@@ -71,21 +74,39 @@ fn cut_system(
 ) {
     for key in keyboard_input.get_just_pressed() {
         if let KeyCode::Return = key {
-            let mut markers: Vec<&PositionMarker> = positions.iter().collect();
-            markers.sort_by_key(|marker| marker.num);
-            let mut excerpts: Vec<&NamedExcerpt> = excerpts.iter().collect();
-            excerpts.sort_by_key(|excerpt| excerpt.num);
-            for ((excerpt_start, excerpt_end), (marker_start, marker_end)) in excerpts
-                .iter()
-                .zip(excerpts[1..].iter())
-                .zip(markers.iter().zip(markers[1..].iter()))
-            {
-                let song = &session.songs[marker_start.num];
-                let start_time = marker_start.get_audio_time(&excerpt_start.excerpt);
-                let end_time = marker_end.get_audio_time(&excerpt_end.excerpt);
-                dbg!(song, start_time, end_time);
-                cut_song(&session, song, start_time, end_time).unwrap();
-            }
+            let cut_info = get_cut_info(&session, &positions, &excerpts);
+            let cloned_session = session.clone();
+            let handle = thread::spawn(move || {
+                for info in cut_info.iter() {
+                    cut_song(&cloned_session, &info.song, info.start_time, info.end_time).unwrap();
+                }
+            });
         }
     }
+}
+
+fn get_cut_info(
+    session: &RecordingSession,
+    positions: &Query<&PositionMarker>,
+    excerpts: &Query<&NamedExcerpt>) -> Vec<CutInfo> {
+        let mut markers: Vec<&PositionMarker> = positions.iter().collect();
+        markers.sort_by_key(|marker| marker.num);
+        let mut excerpts: Vec<&NamedExcerpt> = excerpts.iter().collect();
+        excerpts.sort_by_key(|excerpt| excerpt.num);
+        let mut cut_info: Vec<CutInfo> = vec![];
+        for ((excerpt_start, excerpt_end), (marker_start, marker_end)) in excerpts
+            .iter()
+            .zip(excerpts[1..].iter())
+            .zip(markers.iter().zip(markers[1..].iter()))
+        {
+            let song = &session.songs[marker_start.num];
+            let start_time = marker_start.get_audio_time(&excerpt_start.excerpt);
+            let end_time = marker_end.get_audio_time(&excerpt_end.excerpt);
+            cut_info.push(CutInfo {
+                song: song.clone(),
+                start_time,
+                end_time,
+            });
+        }
+    cut_info
 }
