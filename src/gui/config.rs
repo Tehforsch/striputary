814e3fdb2dc577e80158779c3c709d3622840af1
tehforsch <mail@tonipeter.de>
2021-07-23 16:03:00 +0200

    adjust height

--- src/gui/config.rs
+++ src/gui/config.rs
@@ -2,11 +2,11 @@ pub static SONG_X_START: f32 = -200.0;
 pub static SONG_X_END: f32 = 200.0;
 pub static SONG_TEXT_X_DISTANCE: f32 = 150.0;
 
-pub static SONG_Y_START: f32 = -300.0;
+pub static SONG_Y_START: f32 = -100.0;
 pub static SONG_HEIGHT: f32 = 100.0;
 pub static Y_DISTANCE_PER_MOUSEWHEEL_TICK: f32 = 60.0;
 pub static Y_OFFSET_PER_SONG: f32 = -120.0;
-pub static SONG_TEXT_Y_OFFSET: f32 = -300.0;
+pub static SONG_TEXT_Y_OFFSET: f32 = -100.0;
 
 pub static LINE_WIDTH: f32 = 5.0;
 pub static MARKER_HEIGHT: f32 = 60.0;
